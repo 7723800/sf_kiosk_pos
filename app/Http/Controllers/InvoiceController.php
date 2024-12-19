1918e807e5a80030e15b2a520aeb27b9ef33db59
sergeytalko <7723800@mail.ru>
2024-12-19 10:21:10 +0500

    download improvements

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,17 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Rawilk\Printing\Facades\Printing;
 use Illuminate\Support\Facades\Storage;
 
 class InvoiceController extends Controller
 {
-    public function printInvoice(Request $request)
+    /**
+     * The number of attempts to retrying.
+     *
+     * @var int
+     */
+    private $counter = 0;
+
+    /**
+     * Download and print invoice.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function printInvoice(Request $request): JsonResponse
     {
+        if ($this->counter == 3) {
+            return response()->json();
+        }
+
+        $this->counter++;
         $filePath = parse_url($request->invoiceUrl, PHP_URL_PATH);
         $parts = explode("/", $filePath);
         $invoiceName = end($parts);
-        Storage::disk('public')->put("/invoices/$invoiceName", file_get_contents($request->invoiceUrl));
+
+        try {
+            Storage::disk('public')->put("/invoices/$invoiceName", file_get_contents($request->invoiceUrl));
+        } catch (\Throwable $th) {
+            report($th);
+            sleep(10);
+            return $this->printInvoice($request);
+        }
 
         $printJob = Printing::newPrintTask()
             ->printer(env('CUPS_PRINTER_ID'))
